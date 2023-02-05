140f052e95e786c7fadb005a267420a9e886a52b
Randy Cisneros <rlcisneros90@gmail.com>
2023-02-05 10:28:53 -0700

    Add solutions for meal and order files

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,58 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+return (`Delicious ${name}`)
 }
 
+function createMenuItem(foodName, foodPrice, foodType) {
+  var menu = {
+    name: foodName,
+    price: foodPrice,
+    type: foodType
+  }
+  return menu
+} 
+
+function addIngredients(ingredient, ingredientList) {
+  for (var i = 0; i < ingredientList.length; i++) {
+    if (ingredientList[i] === ingredient) {
+      return ingredientList;
+    }
+  }
+  ingredientList.push(ingredient);
+  return ingredientList;
+}
+
+// function refundOrder(orderNumber, deliveryOrders) {
+//   for (var i = 0; i < deliveryOrders.length; i++) {
+//     if (deliveryOrders[i].orderNumber === orderNumber) {
+//       return deliveryOrders.splice(i, 1);
+//     }
+//   }
+// } 
+
+function formatPrice(cost, fullCost) {
+ return fullCost = `$${cost}`
+}
+
+function decreasePrice(price, reducedPrice) {
+ return reducedPrice = price * .9
+}
+
+function createRecipe(foodTitle, foodIngredients, menuItemType) {
+  var recipe = {
+    title: foodTitle,
+    ingredients: foodIngredients,
+    type: menuItemType
+  }
+  return recipe
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
--- src/order.js
+++ src/order.js
@@ -1,7 +1,32 @@
+function takeOrder (orderNum, deliveryOrders) {
+  if (deliveryOrders.length >= 3) {
+    return deliveryOrders;
+  }
+  deliveryOrders.push(orderNum);
+  return deliveryOrders;
+}
+
+function refundOrder(orderNumber, deliveryOrders) {
+for (var i = 0; i < deliveryOrders.length; i++) {
+  if (deliveryOrders[i].orderNumber === orderNumber) {
+    return deliveryOrders.splice(i, 1)  
+  }
+ }
+}
+
+function listItems (item, orderNumber) {
+  var newString = ''
+  if (orderNumber === orderNumber) {
+   console.log(newString.concat(item))
+  }
+//  var newString = ''
+//  newString = newString + item 
+//  console.log(newString)
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
+  takeOrder,
+  refundOrder,
+  listItems,
   // searchOrder
 }
