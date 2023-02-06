aad8cb23b1354a69b80bb40411993ffc35f4ba9f
Randy Cisneros <rlcisneros90@gmail.com>
2023-02-05 18:56:02 -0700

    Add new solutions to restaurant

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,34 @@
+function createRestaurant (restaurantName) {
+return  {
+  name: restaurantName,
+  menus: {
+    breakfast: [],
+    lunch: [],
+    dinner: []
+  }
+}
 
+}
+
+function addMenuItem(restaurant, foodItem) {
+  for (var i = 0; i < restaurant.menus[foodItem.type].length; i++) {
+    if (restaurant.menus[foodItem.type][i] === foodItem) {
+      return restaurant.menus;
+    }
+  }
+  restaurant.menus[foodItem.type].push(foodItem);
+  return restaurant.menus;
+}
+
+function removeMenuItem(restaurant, foodItem, type) {
+  for (var i = 0; i < restaurant.menus[type].length; i++) {
+    if (restaurant.menus[type][i] === type)
+      return restaurant.menus[type].splice(i, 0) 
+  }
+}
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant, 
+  addMenuItem,
+  removeMenuItem
 }
